d655d40de61a69db6e848ca7724ff938c28845c8
Hwi1005 <jeny-gyrenkova2012@yandex.ru>
2022-11-26 21:42:29 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    println("First side")
+    val side1 = readln().toInt()
+    println("Second side")
+    val side2 = readln().toInt()
+    println("Third side")
+    val side3 = readln().toInt()
+    val velum = side1 * side2 * side3
+    println("Result: ")
+    println(velum)
+
+
+}
+
